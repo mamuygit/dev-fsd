3a556d4fccc5b41fecb15a948340802ac905eed3
mamuygit <mamuy7@gmail.com>
2016-04-21 10:03:53 +0700

    add php file

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// Replace with your own
+$sUsername = 'b51ae984079fde';
+$sPassword = 'e90b9573';
+$sHost = 'ap-cdbr-azure-southeast-b.cloudapp.net';
+$sDb = 'wpdevfsd';
+?>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>test</title>
+  </head>
+  <body>
+    Hello World !!!!
+    FSD
+    <?php
+      require_once 'config.php';
+      try {
+          $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+          $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          echo($oConn)
+          // $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
+          // $oResult = $oStmt->fetchAll();
+          //
+          // foreach ($oResult as $aRow) {
+          //     print_r($aRow['data']);
+          // }
+
+      } catch(PDOException $e) {
+          echo 'ERROR: ' . $e->getMessage();
+      }
+    ?>
+  </body>
+</html>
