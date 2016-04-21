1f9416fb4a2f7a147e53fe6069b015e491ebca04
mamuygit <mamuy7@gmail.com>
2016-04-21 10:35:11 +0700

    test create table

--- index.php
+++ index.php
@@ -10,29 +10,33 @@
     <?php
       require_once 'config.php';
       // require_once 'config.php';
-      define('DB_NAME', getenv('DBNAME'));
-      /** MySQL database username */
-      define('DB_USER', getenv('DBUSER'));
-      /** MySQL database password */
-      define('DB_PASSWORD', getenv('DBPASSWORD'));
-      /** MySQL hostname */
-      define('DB_HOST', getenv('DBHOST'));
-      $conn_str = getenv("MYSQLCONNSTR_consfsd");
-      echo($DB_NAME);
-      // try {
-      //     $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
-      //     $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      //     //echo($oConn)
-      //     // $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
-      //     // $oResult = $oStmt->fetchAll();
-      //     //
-      //     // foreach ($oResult as $aRow) {
-      //     //     print_r($aRow['data']);
-      //     // }
-      //
-      // } catch(PDOException $e) {
-      //     echo 'ERROR: ' . $e->getMessage();
-      // }
+      try {
+          $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+          // sql to create table
+          $sql = "CREATE TABLE MyGuests (
+          id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+          firstname VARCHAR(30) NOT NULL,
+          lastname VARCHAR(30) NOT NULL,
+          email VARCHAR(50),
+          reg_date TIMESTAMP
+          )";
+
+          // use exec() because no results are returned
+          $conn->exec($sql);
+          echo "Table MyGuests created successfully";
+          //echo($oConn)
+          // $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
+          // $oResult = $oStmt->fetchAll();
+          //
+          // foreach ($oResult as $aRow) {
+          //     print_r($aRow['data']);
+          // }
+
+      } catch(PDOException $e) {
+          echo 'ERROR: ' . $e->getMessage();
+      }
     ?>
   </body>
 </html>
