fbc9845780a2de7997879cf9e5bef94395794a2a
mamuygit <mamuy7@gmail.com>
2016-04-21 10:06:33 +0700

    update php

--- index.php
+++ index.php
@@ -8,21 +8,22 @@
     Hello World !!!!
     FSD
     <?php
-      require_once 'config.php';
-      try {
-          $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
-          $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          echo($oConn)
-          // $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
-          // $oResult = $oStmt->fetchAll();
-          //
-          // foreach ($oResult as $aRow) {
-          //     print_r($aRow['data']);
-          // }
-
-      } catch(PDOException $e) {
-          echo 'ERROR: ' . $e->getMessage();
-      }
+    echo('hello');
+      // require_once 'config.php';
+      // try {
+      //     $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+      //     $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      //     echo($oConn)
+      //     // $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
+      //     // $oResult = $oStmt->fetchAll();
+      //     //
+      //     // foreach ($oResult as $aRow) {
+      //     //     print_r($aRow['data']);
+      //     // }
+      //
+      // } catch(PDOException $e) {
+      //     echo 'ERROR: ' . $e->getMessage();
+      // }
     ?>
   </body>
 </html>
