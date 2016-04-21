6a10a7bf1664580350306a2edd55ef7b67a03cd4
mamuygit <mamuy7@gmail.com>
2016-04-21 10:39:42 +0700

    test

--- index.php
+++ index.php
@@ -15,14 +15,14 @@
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
           // sql to create table
-          $sql = "CREATE TABLE MyGuests (
-          id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-          firstname VARCHAR(30) NOT NULL,
-          lastname VARCHAR(30) NOT NULL,
-          email VARCHAR(50),
-          reg_date TIMESTAMP
-          )";
-
+          // $sql = "CREATE TABLE MyGuests (
+          // id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+          // firstname VARCHAR(30) NOT NULL,
+          // lastname VARCHAR(30) NOT NULL,
+          // email VARCHAR(50),
+          // reg_date TIMESTAMP
+          // )";
+          $sql = "SELECT LastName FROM MyGuests";
           // use exec() because no results are returned
           $conn->exec($sql);
           echo "Table MyGuests created successfully";
