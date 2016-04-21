e4190751cbe51b40ef2e849b91a8b29ad920aee0
mamuygit <mamuy7@gmail.com>
2016-04-21 10:09:45 +0700

    add connect db

--- index.php
+++ index.php
@@ -9,23 +9,21 @@
     FSD
     <?php
       require_once 'config.php';
-      echo('hello');
-      echo($sHost);
       // require_once 'config.php';
-      // try {
-      //     $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
-      //     $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      //     //echo($oConn)
-      //     // $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
-      //     // $oResult = $oStmt->fetchAll();
-      //     //
-      //     // foreach ($oResult as $aRow) {
-      //     //     print_r($aRow['data']);
-      //     // }
-      //
-      // } catch(PDOException $e) {
-      //     echo 'ERROR: ' . $e->getMessage();
-      // }
+      try {
+          $oConn = new PDO('mysql:host='.$sHost.';dbname='.$sDb, $sUsername, $sPassword);
+          $oConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          //echo($oConn)
+          // $oStmt = $oConn->prepare('SELECT data FROM `hello_world`');
+          // $oResult = $oStmt->fetchAll();
+          //
+          // foreach ($oResult as $aRow) {
+          //     print_r($aRow['data']);
+          // }
+
+      } catch(PDOException $e) {
+          echo 'ERROR: ' . $e->getMessage();
+      }
     ?>
   </body>
 </html>
